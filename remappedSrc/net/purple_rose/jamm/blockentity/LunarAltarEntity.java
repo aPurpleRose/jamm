c99d7f4b9c235cb200edeae281c498316c0c8711
D4rkR0y <92951794+aPurpleRose@users.noreply.github.com>
2021-10-24 04:07:03 +0200

    .

---
+++ remappedSrc/net/purple_rose/jamm/blockentity/LunarAltarEntity.java
@@ -0,0 +1,32 @@
+package net.purple_rose.jamm.blockentity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class LunarAltarEntity extends BlockEntity {
+    public LunarAltarEntity(BlockPos pos, BlockState state) {
+        super(ModBlockEntities.LUNAR_ALTAR_ENTITY, pos, state);
+    }
+
+    private int number = 0;
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        nbt.putInt("number", number);
+        return nbt;
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        number = nbt.getInt("number");
+    }
+
+    public static void tick(World world, BlockPos pos, BlockState state, LunarAltarEntity lwe){
+        lwe.number++;
+    }
+}
---
+++ remappedSrc/net/purple_rose/jamm/item/custom/TutorialBookItem.java
@@ -0,0 +1,12 @@
+package net.purple_rose.jamm.item.custom;
+
+import net.minecraft.item.WrittenBookItem;
+
+public class TutorialBookItem extends WrittenBookItem {
+    public TutorialBookItem(Settings settings) {
+        super(settings);
+    }
+
+
+
+}
