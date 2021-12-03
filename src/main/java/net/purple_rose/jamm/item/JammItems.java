3836421065d61bf6f1c0aa99b3d9f35c2ed28fb7
D4rkR0y <92951794+aPurpleRose@users.noreply.github.com>
2021-12-03 19:23:02 +0100

    .

--- src/main/java/net/purple_rose/jamm/item/JammItems.java
+++ src/main/java/net/purple_rose/jamm/item/JammItems.java
@@ -2,18 +2,35 @@ package net.purple_rose.jamm.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.purple_rose.jamm.Jamm;
-import net.purple_rose.jamm.item.custom.AmethystWandItem;
-import net.purple_rose.jamm.item.custom.DarkAmethystWandItem;
-import net.purple_rose.jamm.item.custom.DowsingRodItem;
-import net.purple_rose.jamm.item.custom.TutorialBookItem;
+import net.purple_rose.jamm.item.custom.*;
 
 public class JammItems {
 
     public static final Item AMETHYST = registerItem("amethyst",
             new Item(new FabricItemSettings().group(JammItemGroup.JAMM)));
+
+    public static final Item AMETHYST_AXE = registerItem("amethyst_axe",
+            new AmethystAxeItem(JammToolMaterial.AMETHYST, 2.5f, 1f,
+                    new FabricItemSettings().group(JammItemGroup.JAMM)));
+    public static final Item AMETHYST_HOE = registerItem("amethyst_hoe",
+            new AmethystHoeItem(JammToolMaterial.AMETHYST, 2, 1f,
+                    new FabricItemSettings().group(JammItemGroup.JAMM)));
+    public static final Item AMETHYST_PICKAXE = registerItem("amethyst_pickaxe",
+            new AmethystPickaxeItem(JammToolMaterial.AMETHYST, 2, 1f,
+                    new FabricItemSettings().group(JammItemGroup.JAMM)));
+
+    public static final Item AMETHYST_SHOVEL = registerItem("amethyst_shovel",
+            new ShovelItem(JammToolMaterial.AMETHYST, 2.5f, 1f,
+                    new FabricItemSettings().group(JammItemGroup.JAMM)));
+    public static final Item AMETHYST_SWORD = registerItem("amethyst_sword",
+            new SwordItem(JammToolMaterial.AMETHYST, 2, 1f,
+                    new FabricItemSettings().group(JammItemGroup.JAMM)));
+
     public static final Item AMETHYST_WAND = registerItem("amethyst_wand",
             new AmethystWandItem(new FabricItemSettings().group(JammItemGroup.JAMM).maxCount(1)));
     public static final Item DARK_AMETHYST = registerItem("dark_amethyst",
---
+++ src/main/java/net/purple_rose/jamm/item/custom/AmethystAxeItem.java
@@ -0,0 +1,10 @@
+package net.purple_rose.jamm.item.custom;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class AmethystAxeItem extends AxeItem {
+    public AmethystAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
