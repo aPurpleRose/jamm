45f39330b4fa94e2108a3d03934f0598141c55e4
aPurpleRose <92951794+aPurpleRose@users.noreply.github.com>
2022-03-22 18:19:55 +0100

    1.18.2 update

--- src/main/java/io/github/apurplerose/jamm/util/JammTags.java
+++ src/main/java/io/github/apurplerose/jamm/util/JammTags.java
@@ -1,23 +1,23 @@
 package io.github.apurplerose.jamm.util;
 
 import io.github.apurplerose.jamm.Jamm;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class JammTags {
     // https://fabricmc.net/wiki/tutorial:tags for common tags
     public static class Blocks {
-        public static final Tag.Identified<Block> VALUABLE_BLOCKS = createTag("valuable_blocks");
+        public static final TagKey<Block> VALUABLE_BLOCKS = createTag("valuable_blocks");
 
-        private static Tag.Identified<Block> createTag(String name) {
-            return TagFactory.BLOCK.create(new Identifier("c", name));
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
         }
 
-        private static Tag.Identified<Block> createCommonTag(String name) {
-            return TagFactory.BLOCK.create(new Identifier("c", name));
+        private static TagKey<Block> createCommonTag(String name) {
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
         }
     }
 
@@ -25,12 +25,12 @@ public class JammTags {
 
 
 
-        private static Tag.Identified<Item> createTag(String name) {
-            return TagFactory.ITEM.create(new Identifier(Jamm.MOD_ID, name));
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(Registry.ITEM_KEY, new Identifier(Jamm.MOD_ID, name));
         }
 
-        private static Tag.Identified<Item> createCommonTag(String name) {
-            return TagFactory.ITEM.create(new Identifier(Jamm.MOD_ID, name));
+        private static TagKey<Item> createCommonTag(String name) {
+            return TagKey.of(Registry.ITEM_KEY, new Identifier(Jamm.MOD_ID, name));
         }
     }
 }
