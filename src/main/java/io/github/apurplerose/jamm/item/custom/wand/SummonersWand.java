ec9befb0a7b639e59932931d2975585138adeadb
aPurpleRose <alice.purple-rose@outlook.com>
2022-06-11 01:45:31 +0200

    small note

--- src/main/java/io/github/apurplerose/jamm/item/custom/wand/SummonersWand.java
+++ src/main/java/io/github/apurplerose/jamm/item/custom/wand/SummonersWand.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class SummonersWand extends WandItem {
@@ -13,9 +14,13 @@ public class SummonersWand extends WandItem {
         @Override
         public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
                 ItemStack stack = user.getStackInHand(hand);
+                Vec3d pos = user.getPos();
+
+                // pitch (left right) and yaw (up down)
 
                 FireballEntity type = EntityType.FIREBALL.create(world);
-                type.setPos(user.getPos().getX(), user.getPos().getY(), user.getPos().getZ());
+                type.setPos(pos.getX(), pos.getY() + 1, pos.getZ());
+                type.setVelocity(0, -1, 0);
                 world.spawnEntity(type);
 
                 return TypedActionResult.success(stack);
